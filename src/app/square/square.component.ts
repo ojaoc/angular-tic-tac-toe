d86e0fa18ea27ce58218b2c2797fe52ab22e1c4c
João Correia <correia.jafonso@gmail.com>
2021-12-05 18:34:50 +0000

    Functional game, pwa support

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,11 +2,32 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: ` <button>{{ value }}</button> `,
-  styles: [],
+  template: `
+    <button nbButton *ngIf="!value" [disabled]="gameOver">{{ value }}</button>
+    <button
+      nbButton
+      hero
+      status="success"
+      *ngIf="value === 'X'"
+      [disabled]="gameOver"
+    >
+      {{ value }}
+    </button>
+    <button
+      nbButton
+      hero
+      status="info"
+      *ngIf="value === 'O'"
+      [disabled]="gameOver"
+    >
+      {{ value }}
+    </button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
+  @Input() gameOver: boolean;
 
   constructor() {}
 }
